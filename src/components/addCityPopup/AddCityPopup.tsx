daa3f5be9b9b6f8b2f0f6236445787e9d74418eb
Yurii Prystai <yurayprystay@gmail.com>
2022-05-03 23:06:10 +0300

    fix: performance of adding new city

--- src/components/addCityPopup/AddCityPopup.tsx
+++ src/components/addCityPopup/AddCityPopup.tsx
@@ -3,32 +3,28 @@ import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
-import { nanoid } from 'nanoid'
 import {
   CircularProgress,
   FormControl,
   IconButton,
-  InputLabel,
-  MenuItem,
-  Select,
-  SelectChangeEvent,
 } from '@mui/material';
 import CloseIcon from '@mui/icons-material/Close';
 import { connect, useDispatch } from 'react-redux';
 import { ICity } from 'country-state-city/dist/lib/interface';
+import AsyncSelect from "react-select/async";
+import { createFilter, SingleValue } from 'react-select';
+import { City } from 'country-state-city';
+import VirtualList from 'react-tiny-virtual-list';
 
 import { AddCityPopupProps, RootState } from '../../config/types';
 import { closeButtonStyle, modalStyle } from './addCityPopupStyles';
 import { fetchCityToAdd } from '../../redux/thunks';
+import { setWorldsCities } from '../../redux/actionCreators';
 
 const AddCityPopup = (props: AddCityPopupProps) => {
   const dispatch = useDispatch();
   const [cityName, setCityName] = React.useState<string>('');
   const [isLoading, setIsLoading] = React.useState<boolean>(!props.cities);
-
-  const handleChange = (event: SelectChangeEvent): void => {
-    setCityName(event.target.value as string);
-  };
 
   const handleAddCity = (): void => {
     const selectedCity: ICity | undefined = props.cities.find(city => city.name === cityName);
@@ -41,7 +37,50 @@ const AddCityPopup = (props: AddCityPopupProps) => {
       ));
       setCityName('');
     }
-  };  
+  };
+
+  const loadOptions = (
+    inputValue: string,
+    callback: (options: ICity[]) => void
+  ) => {
+    let cities: ICity[] | undefined = [...props.cities];
+    if (!cities.length) {      
+      cities = City.getCitiesOfCountry('UA');
+      if (cities) {
+        cities = cities.map(city => {
+          return {
+            ...city,
+            label: city.name,
+            value: city.name,
+          }
+        });
+        dispatch(setWorldsCities(cities));
+        callback(cities);
+      }
+    } else {
+      callback(cities.map(city => {
+        return {
+          ...city,
+          label: city.name,
+          value: city.name,
+        }
+      }));
+    }
+  };
+
+  const MenuList = (props: any) => (
+    <VirtualList
+      width="100%"
+      height={300}
+      itemCount={props.options.length}
+      itemSize={35}
+      renderItem={({ index, style }) => (
+        <div key={index} style={style}>
+          {React.Children.toArray(props.children)[index]}
+        </div>
+      )}
+    />
+  );
 
   return (
     <div>
@@ -68,25 +107,23 @@ const AddCityPopup = (props: AddCityPopupProps) => {
           <Typography align='center' variant="h6" component="h2" sx={{ mb: 2 }}>
             Select a new City to add
           </Typography>
-          <Box  sx={{ mb: 2 }}>
+          <Box>
             {
               isLoading
               ? <CircularProgress />
               : (
                 <FormControl fullWidth sx={{ mb: 3 }}>
-                  <InputLabel id="demo-simple-select-label">City</InputLabel>
-                  <Select
-                    labelId="demo-simple-select-label"
-                    value={cityName}
-                    label="City"
-                    onChange={handleChange}
-                  >
-                    {
-                      props.cities.map((city) => {
-                        return <MenuItem key={nanoid()} value={city.name}>{city.name}</MenuItem>
-                      })
-                    }
-                  </Select>
+                  <AsyncSelect
+                    components={{ MenuList }}
+                    cacheOptions
+                    defaultOptions
+                    loadOptions={loadOptions}
+                    onChange={(newValue: SingleValue<ICity>) => {   
+                      setCityName(newValue ? newValue.name : '');
+                    }}
+                    isSearchable={false}
+                    filterOption={createFilter({ ignoreAccents: false })}
+                  />
                 </FormControl>
               )
             }
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -5,12 +5,9 @@ import {
   Container,
 } from '@mui/material'
 import React, { useEffect, useState } from 'react'
-import { City }  from 'country-state-city';
 import { useDispatch, connect } from 'react-redux';
-import { ICity } from 'country-state-city/dist/lib/interface';
 
 import AddCityPopup from '../components/addCityPopup/AddCityPopup';
-import { setWorldsCities } from '../redux/actionCreators';
 import CitiesList from '../components/CitiesList';
 import { MainPageProps, RootState } from '../config/types';
 import { fetchAllCitiesToRefresh } from '../redux/thunks';
@@ -23,10 +20,6 @@ const MainPage = (props: MainPageProps) => {
   const handleClose: () => void = () => setPopupIsOpen(false);
 
   useEffect(() => {
-    if (!props.worldsCities.length) {
-      const cities: ICity[] | undefined = City.getCitiesOfCountry('UA');  
-      dispatch(setWorldsCities(cities));
-    }
     if (props.weatherCities.length) {
       dispatch(fetchAllCitiesToRefresh(
         props.weatherCities,
